91f3a0c37b03d38f521c828d516cb6ef490c09fa
mbuiiiz <ductai2068@gmail.com>
2026-03-01 09:04:43 -0800

    fixed layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "@/pages/Index";
-import Session from "@/pages/Session";
-import ExerciseGallery from "@/pages/ExerciseGallery";
+import Index from "./pages/Index";
+import Session from "./pages/Session";
+import ExerciseGallery from "./pages/ExerciseGallery";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -17,6 +17,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/session" element={<Session />} />
+          <Route path="/gallery" element={<ExerciseGallery />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
